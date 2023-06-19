b2ee0ce571ea1620626a1555f8d1b1b5bc236887
张涵 <zhanghan@qmai.cn>
2023-06-19 21:57:46 +0800

    test: 1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  onLaunch() {
+    console.log('App Launch')
+  },
+  onShow() {
+    console.log('App Show')
+  },
+  onHide() {
+    console.log('App Hide')
+  },
+}
+</script>
+
+<style lang="scss">
+/*每个页面公共css */
+
+.a {
+  position: absolute;
+  height: 100%;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import uni from '@dcloudio/vite-plugin-uni'
+import { defineConfig } from 'vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [uni()],
+})
